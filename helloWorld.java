7056b8c3f07917fc0ffdba6c621b17da7ff7b5bd
courts188 <79217452+courts188@users.noreply.github.com>
2021-02-18 19:41:41 +0000

    hope

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,8 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-     System.out.println("Test1");  
+class Simple {
+    public static void main(String args[]) {
+        System.out.println("Hello Java");
+        System.out.println("Test1");
+        System.out.println("Might have the hand of this");
 
-    }  
+    }
 }
